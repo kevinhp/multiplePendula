fccbb5b0ae2aab07d5630642d2972d1f50845d87
KHernandez <khernandezpardo@gmail.com>
2018-02-17 23:30:00 -0600

    p5 canvas switched to implementation instead of global so as to allowed better control of the web page design.

--- pendulum.js
+++ pendulum.js
@@ -1,6 +1,6 @@
 
 class Pendulum {
-    constructor(angles,lengths,masses) {
+    constructor(angles,lengths,masses,canvas) {
         if (!angles) {
             angles = [Math.PI/4,Math.PI/8];
             lengths = [100,100];
@@ -21,34 +21,35 @@ class Pendulum {
         if (!g) {
             this.g = 1;
         }
+        this.canvas = canvas;
         this.g = g;
         this.n = angles.length;
-
+        
         this.s = math.matrix(angles); // State: Column vector of angles followed by their rates
         this.s.resize([2*this.n],0);
         this.ang = this.s.subset(math.index(math.range(0,this.n))).valueOf();
         this.angd = this.s.subset(math.index(math.range(this.n,2*this.n))).valueOf();
-
+        
         this.lens = lengths;
         var rm_scale = 15; // Proportion between mass and radius^2
         this.masses = masses;
         this.rs = math.multiply(math.sqrt(this.masses),rm_scale); // Radii
-
+        
         this.T = math.zeros(this.n);
         this.posHistory = [];
         this.histCount = 0;
-
+        
         this.integrateStep = this.rk4Step;
     }
-
+    
     useRK4() {
         this.integrateStep = this.rk4Step;
     }
-
+    
     useEuler() {
         this.integrateStep = this.eulerStep;
     }
-
+    
     update() {
         var h = 0.05;
         var n = 20;
@@ -62,7 +63,7 @@ class Pendulum {
             }
         }
     }
-
+    
     show() {
         // Get XY coordinates
         var x = [0];
@@ -71,79 +72,82 @@ class Pendulum {
             x[i+1] = x[i] + this.lens[i]*math.cos(this.ang[i]);
             y[i+1] = y[i] + this.lens[i]*math.sin(this.ang[i]);
         }
-
+        
         // Pendula are currently paused initially until clicked on. This forces getting the energy for the display
-        if (frameCount==1) {
+        if (this.canvas && this.canvas.frameCount==1) {
             this.getEnergy(y);
         }
-
+        
         // b = true: pendula in action. b = false: pendula paused
         // Update trace and energy only if pendula are in action
         if (b) {
             this.histCount++; // Not using it but I thought it might be handy in the future
             this.posHistory.push([x[this.n],y[this.n]]);
-
+            
             // Keep only up to 500 registers
             if (this.posHistory.length>500) {
                 this.posHistory.shift();
             }
-
+            
             this.getEnergy(y);        
         }
-
+        
+        if (!this.canvas) {
+            return;
+        }
         // Draw pendula lines and circles
         for (var i = 1; i <= this.n; i++) {
-            stroke(200);
-            strokeWeight(2);
-            line(x[i-1],y[i-1],x[i],y[i]);
-            noStroke();
-            fill('rgba(0,0,255,0.8)');
-            ellipse(x[i],y[i],this.rs[i-1],this.rs[i-1]);
+            this.canvas.stroke(200);
+            this.canvas.strokeWeight(2);
+            this.canvas.line(x[i-1],y[i-1],x[i],y[i]);
+            this.canvas.noStroke();
+            this.canvas.fill('rgba(0,0,255,0.8)');
+            this.canvas.ellipse(x[i],y[i],this.rs[i-1],this.rs[i-1]);
         }
-
+        
         // Draw trace
-        noFill();
-        stroke('rgba(255,0,0,1)');
-        strokeWeight(1);
-        beginShape();
+        this.canvas.noFill();
+        this.canvas.stroke('rgba(255,0,0,1)');
+        this.canvas.strokeWeight(1);
+        this.canvas.beginShape();
         for (var i = 1; i < this.posHistory.length; i++) {
-            vertex(this.posHistory[i][0],this.posHistory[i][1]);
+            this.canvas.vertex(this.posHistory[i][0],this.posHistory[i][1]);
         }
-        endShape();
+        this.canvas.endShape();
     }
-
+    
     eulerStep(h) {
-    /*
+        /*
         Euler method implementation
         h: step size
-    */
+        */
         var y = this.s.clone();
         var k = this.evaluateAt(y);
         this.s = math.add(this.s,math.multiply(k,h));
-
+        
     }
-
+    
     rk4Step(h) {
-    /*
+        /*
         4-th order Runge-Kutta implementation
         h: step size
-    */
+        */
         var y = this.s.clone();
         var k1 = this.evaluateAt(y);
         var k2 = this.evaluateAt(math.add(y,math.multiply(k1,0.5*h)));
         var k3 = this.evaluateAt(math.add(y,math.multiply(k2,0.5*h)));
         var k4 = this.evaluateAt(math.add(y,math.multiply(k3,    h)));
- 
+        
         this.s = math.add(this.s,math.multiply((math.add(math.add(k1,math.multiply(k2,2)), math.add(math.multiply(k3,2), k4))),h/6));
     }
-
+    
     evaluateAt(y) {
-    /*
+        /*
         y: state, column vector containing 'ang' followed by 'angd' (read below)
         Derivative is not an explicit function of time, so I am not requiring that input.
-
+        
         Matrices A, B, C, and D are used in the derivation of the equations:
-         A.angdd - B.angd2 = wc + D.t
+        A.angdd - B.angd2 = wc + D.t
         -B.angdd - A.angd2 = ws + C.t
         Angles are measured from horizontal-right and positive in clock-wise direction
         ang:   vector containing angular positions
@@ -153,15 +157,15 @@ class Pendulum {
         wc:    vector containing tangential component of weight (respect to their local polar frame)
         ws:    vector containing radial component of weight (respect to their local polar frame)
         t:     vector containing the value of tension by string connecting to previous particle
-    */
+        */
         var A = math.zeros(this.n,this.n); 
         var B = math.zeros(this.n,this.n);
         var C = math.zeros(this.n,this.n);
         var D = math.zeros(this.n,this.n);
-
+        
         var ang  = y.subset(math.index(math.range(0     ,  this.n))).valueOf();
         var angd = y.subset(math.index(math.range(this.n,2*this.n))).valueOf();
-
+        
         // When n == 1 ang and angd are coming out as scalars and that's breaking several things.
         // These n==1 conditionals are patches
         if (this.n == 1) {
@@ -169,7 +173,7 @@ class Pendulum {
             angd = [this.angd];
         }
         var angd2 = math.square(angd);
-
+        
         // Fill vectors
         var wc;
         var ws;
@@ -180,21 +184,21 @@ class Pendulum {
             var wc = math.multiply(math.dotMultiply(this.masses,math.cos(ang)),this.g);
             var ws = math.multiply(math.dotMultiply(this.masses,math.sin(ang)),this.g);
         }
-
+        
         // Fill matrices
         for (var i = 0; i < this.n; ++i) {
-           A.subset(math.index(i,i),this.lens[i]*this.masses[i]);
+            A.subset(math.index(i,i),this.lens[i]*this.masses[i]);
             for (var j = 0; j < i; ++j) {
-               A.subset(math.index(i,j),this.lens[j]*this.masses[i]*math.cos(ang[j] - ang[i]));
-               B.subset(math.index(i,j),this.lens[j]*this.masses[i]*math.sin(ang[j] - ang[i]));
+                A.subset(math.index(i,j),this.lens[j]*this.masses[i]*math.cos(ang[j] - ang[i]));
+                B.subset(math.index(i,j),this.lens[j]*this.masses[i]*math.sin(ang[j] - ang[i]));
             }
             C.subset(math.index(i,i),-1);
             if (i < this.n-1) { 
-               C.subset(math.index(i,i+1),math.cos(ang[i+1] - ang[i]));
-               D.subset(math.index(i,i+1),math.sin(ang[i+1] - ang[i]));
+                C.subset(math.index(i,i+1),math.cos(ang[i+1] - ang[i]));
+                D.subset(math.index(i,i+1),math.sin(ang[i+1] - ang[i]));
             }
         }
-
+        
         // angdd = (A+D.Cinv.B)inv.[wc - D.Cinv.ws + (B-D.Cinv.A).angd2]
         // Helpers to make angdd more readable
         var Cinv   = math.inv(C);              // Inverse of C
@@ -203,7 +207,7 @@ class Pendulum {
         var DCinvB = math.multiply(DCinv,B);   // D.Cinv.B
         var tmp1   = math.add(A,DCinvB);       // (A+D.Cinv.B)
         var tmp2   = math.subtract(B,DCinvA);  // (B-D.Cinv.A)
-
+        
         // angdd = (tmp1)inv. (wc - DCinv.ws + tmp2.angd2)
         var Y = math.matrix(math.zeros(y.size()));
         Y.subset(math.index(math.range(0,this.n)), math.subset(y,math.index(math.range(this.n,2*this.n))));
@@ -226,7 +230,7 @@ class Pendulum {
         Y.subset(math.index(math.range(this.n,2*this.n)), Y2);
         return Y;
     }
-
+    
     getEnergy(y) {
         // Compute energy
         var k = 0; // Kinetic energy
@@ -235,20 +239,20 @@ class Pendulum {
             var sumr = 0; // Radial component of velocity
             var sumq = 0; // Tangential component of velocity
             for (var j = 0; j <= i; j++) {
-                sumr += this.lens[j]*this.angd[j]*sin(this.ang[j] - this.ang[i]);
-                sumq += this.lens[j]*this.angd[j]*cos(this.ang[j] - this.ang[i]);
+                sumr += this.lens[j]*this.angd[j]*math.sin(this.ang[j] - this.ang[i]);
+                sumq += this.lens[j]*this.angd[j]*math.cos(this.ang[j] - this.ang[i]);
             }
             k += 0.5*this.masses[i]*(sumr*sumr + sumq*sumq);
             v -= this.masses[i]*this.g*y[i+1]; // Negative since y points down
         }
-
+        
         var totalEnergy = k + v;
-
+        
         // Store initial energy
-        if (frameCount == 1) {
+        if (this.canvas && this.canvas.frameCount == 1) {
             this.initialEnergy = totalEnergy;
         }
-
+        
         // Print to document
         document.getElementById("energy").innerHTML = "Total energy: " + totalEnergy.toFixed(6) + ", Kinetic: " + k.toFixed(6) + ", Potential: " + v.toFixed(6) + ", Change from start: " + (this.initialEnergy - totalEnergy).toFixed(6);
     }
--- sketch.js
+++ sketch.js
@@ -1,33 +1,34 @@
-var xo; // x origin
-var yo; // y origin
 var g;
-var p;
+var pl; // pendulum list
 var b;
 
-function setup() {
-    createCanvas(1000,600);
-    xo = width/2;
-    yo = 100;
-    g = 1;
-//     var angles = [math.pi*0.9];
-    var angles = [math.pi*1/7,-math.pi*5/11,math.pi*1.1,0];
-    var lengths = [100,100,100,50];
-    var masses = [4,4,3,4];
-    p = new Pendulum(angles,lengths,masses);
-    b = false;
-    translate(xo,yo);
-    p.show();
-}
-
-function draw() {
-    translate(xo,yo);
-    if (b) {
-        p.update();
+var pendulaCanvas = function(p) {
+    var xo;
+    var yo;
+    p.setup = function() {
+        p.createCanvas(1000,600);
+        xo = p.width/2;
+        yo = 100;
+        g = 1;
+        //     var angles = [math.pi*0.9];
+        var angles = [math.pi*1/7,-math.pi*5/11,math.pi*1.1,0];
+        var lengths = [100,100,100,50];
+        var masses = [4,4,3,4];
+        pl = new Pendulum(angles,lengths,masses,p);
+        b = false;
+        p.translate(xo,yo);
+        pl.show();
     }
-    background(0);
-    p.show();
+    p.draw = function() {
+        p.translate(xo,yo);
+        if (b) {
+            pl.update();
+        }
+        p.background(0);
+        pl.show();
+    }
+    p.mouseClicked = function() {
+        b = !b;
+    }
 }
-
-function mouseClicked() {
-    b = !b;
-}
+var displayCanvas = new p5(pendulaCanvas,'pendulaCanvas')
