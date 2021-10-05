f0d2e65318ad6f5e6c56b1abedb8a97867225924
Kevin Hernandez <khp.cdng@outlook.com>
2021-10-04 21:22:13 -0500

    A file for the parameters

---
+++ constants.js
@@ -0,0 +1,10 @@
+let g = 1;  // gravity
+let pl; // pendulum list
+let b;  // bool to pause/unpause with clicks on the canvas
+let xo;
+let yo;
+let maxPend = 20;
+let numPend;
+let p;
+let wHalf;
+let hHalf;
